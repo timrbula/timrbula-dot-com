05f668c5bfa155051303b63a417c9920936a2907
Timothy Bula <trbula@us.ibm.com>
2024-11-09 17:05:18 -0500

    feat: basic site

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,11 +1,11 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = await getCollection('blog');
+  const blog = await getCollection("blog");
   return rss({
-    title: 'Brutal Blog',
-    description: 'Brutal is a theme for Astro',
+    title: "timrbula.com",
+    description: "Tim's home on the internet",
     stylesheet: false,
     site: context.site,
     items: blog.map((post) => ({
@@ -14,7 +14,7 @@ export async function GET(context) {
       description: post.data.description,
       link: `/blog/${post.slug}/`,
     })),
-    customData: '<language>en-us</language>',
-    canonicalUrl: 'https://brutal.elian.codes',
+    customData: "<language>en-us</language>",
+    canonicalUrl: "https://timrbula.com",
   });
 }
